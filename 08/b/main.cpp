f910be2af981bcd06a8f5e8e8a3a03370d815429
YiShuiHanXL <waffe3waffe3@gmail.com>
2014-06-25 10:47:31 +0800

    open file in writeonly way

---
+++ 08/b/main.cpp
@@ -0,0 +1,23 @@
+#include <QFile>
+#include <QDebug>
+
+int main( void )
+{
+    QFile file( "testfile.txt" );
+
+    if( !file.exists() )
+    {
+        qDebug() << "The file of " << file.fileName() << "does not exist";
+        return -1;
+    }
+
+    if( !file.open(QIODevice::WriteOnly) )
+    {
+        qDebug() << "Cannot write the file";
+        return -1;
+    }
+
+    qDebug() << "Success";
+
+    file.close();
+}
