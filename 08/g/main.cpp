ad1dc74e6995fa0cd1fdff6299ecf257aefe96c3
YiShuiHanXL <waffe3waffe3@gmail.com>
2014-06-25 14:48:24 +0800

    write to xml

---
+++ 08/g/main.cpp
@@ -0,0 +1,35 @@
+#include <QDomDocument>
+#include <QDebug>
+#include <QDomText>
+#include <QTextStream>
+#include <QFile>
+
+int main( void )
+{
+    QDomDocument document;
+    QDomElement d = document.createElement( "Document" );
+    d.setAttribute( "name", "document" );
+
+    QDomElement a = document.createElement( "Author" );
+    a.setAttribute( "name", "author");
+    QDomText text = document.createTextNode( "Some text" );
+
+    document.appendChild(d);
+    d.appendChild(a);
+    d.appendChild(text);
+
+
+    QFile file( "data.xml" );
+    if( !file.open( QIODevice::WriteOnly ) )
+    {
+        qDebug() << "Cannot open the file";
+    }
+
+    QTextStream stream( &file );
+
+    stream << document.toString();
+
+    file.close();
+
+    return 0;
+}
